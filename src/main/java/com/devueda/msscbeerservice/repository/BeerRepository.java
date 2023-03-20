64bf895065babf1b1dbfe14d39a20626e8ba9771
Carlos R. Ueda <carlos.ueda@gmail.com>
2023-03-20 07:23:09 -0300

    Creating BeerRepository

---
+++ src/main/java/com/devueda/msscbeerservice/repository/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.devueda.msscbeerservice.repository;
+
+import com.devueda.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
